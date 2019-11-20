d3f299b9aeadaaf619f40e0011028d0559681584
Iglan Cardeal <cmtcardeal@outlook.com>
2019-11-20 07:57:29 -0300

    Ajustando o diretorio de armazenamento de imagem baseando-se na ano e mes do upload do arquivo.

--- middleware/file-upload-handler.js
+++ middleware/file-upload-handler.js
@@ -1,14 +1,25 @@
 const crypto = require("crypto");
 
+const destinationBasedOnDate = () => {
+  const date = new Date();
+  const currentYear = date.getFullYear().toString();
+  const currentMonth = date.getMonth().toString();
+  const months = ["jan", "feb", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
+  const equivalentMonth = months[currentMonth];
+  return {
+    currentYear, equivalentMonth
+  }
+}
+
 module.exports = multer => {
   // Trata upload de arquivos.
+  const directoryBasedOnDate = destinationBasedOnDate();
   const fileStorage = multer.diskStorage({
-    destination: "app/public/users", // local de armazenameto.
+    destination: `app/public/users/${directoryBasedOnDate.currentYear}/${directoryBasedOnDate.equivalentMonth}`, // local de armazenameto.
     filename: (req, file, callback) => {
-      const currentYear = (new Date().getFullYear()).toString();
       const randomFileName =
-        crypto.randomBytes(16).toString("hex") + Date.now(); // gera codido de tamanho 16 + 
-      callback(null,  randomFileName + ".png");
+        crypto.randomBytes(16).toString("hex") + Date.now(); // gera codido de tamanho 16 +
+      callback(null, randomFileName + ".png");
       // callback(null, new Date().toISOString() + "-" + file.originalname); // nome do arquivo inclui data e hora pra diferenciar os nomes.
     }
   });
